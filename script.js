1da68b6b5b91ac6262a6add39efdf6988ae3ee38
Egor Surov <surovegor.se@gmail.com>
2020-05-07 12:47:03 +0400

    Структура

---
+++ script.js
@@ -0,0 +1,8 @@
+let search = document.getElementById("search"),
+    input = document.getElementById("input");
+
+
+  search.addEventListener("click", function(){
+    input.classList.add("open");
+  });
+
